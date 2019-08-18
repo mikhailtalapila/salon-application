d02dd41a3aa65de55d3b11a4cb493aaabbd56b4c
Mikhail Talapila <mikhailtalapila@yahoo.com>
2019-08-17 22:10:36 -0400

    Added the route changing indicators

--- src/app/salon-app.component.ts
+++ src/app/salon-app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { SharedSettingsService } from './shared/services/shared-settings.service';
+import { Router, NavigationStart, NavigationCancel, NavigationEnd, NavigationError, Event } from '@angular/router';
 
 @Component({
   selector: 'salon-app',
@@ -7,4 +7,22 @@ import { SharedSettingsService } from './shared/services/shared-settings.service
   styleUrls: ['./salon-app.component.css']
 })
 export class SalonAppComponent {
+  loading: boolean;
+  constructor(
+    private router: Router
+  ) {
+    router.events.subscribe((value: Event) => {
+      this.checkRouterEvent(value);
+    });
+  } 
+  checkRouterEvent(routerEvent: Event): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    }
+    if (routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError) {
+        this.loading = false;
+      }
+  }
 }
